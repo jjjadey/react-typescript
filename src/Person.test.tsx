cdc8757a43dc8fa34e39b5c6becad7e9eba37367
jjjadey <nattakan.premp@gmail.com>
2022-07-07 13:53:57 +0700

    Basic component testing

---
+++ src/Person.test.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import Person from "./Person";
+
+test("renders learn react link", () => {
+  render(<Person name="John" />);
+
+  // const linkElement = screen.getByText(/Name is John/i);
+  // expect(linkElement).toBeInTheDocument();
+
+  const divElement = screen.getByRole("contentinfo");
+  expect(divElement).toHaveTextContent("Name is John");
+  expect(divElement).toHaveAttribute("role", "contentinfo");
+});
