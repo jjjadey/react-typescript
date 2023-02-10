70e5f87cf69ce8ae1d5a04b41f8d4e041f13076d
jjjadey <nattakan.premp@gmail.com>
2023-02-10 20:14:58 +0700

    [react-testing]: group tests

--- react-testing/src/components/greet.test.tsx
+++ react-testing/src/components/greet.test.tsx
@@ -1,14 +1,16 @@
 import { render, screen } from "@testing-library/react";
 import Greet from "./greet";
 
-test("Greet render correctly", () => {
-  render(<Greet />);
-  const textElement = screen.getByText("Hello");
-  expect(textElement).toBeInTheDocument();
-});
+describe("Greet", () => {
+  test("Greet render correctly", () => {
+    render(<Greet />);
+    const textElement = screen.getByText("Hello");
+    expect(textElement).toBeInTheDocument();
+  });
 
-test("Greet render with a name", () => {
-  render(<Greet name="Jaejoong" />);
-  const textElement = screen.getByText("Hello Jaejoong");
-  expect(textElement).toBeInTheDocument();
+  test("Greet render with a name", () => {
+    render(<Greet name="Jaejoong" />);
+    const textElement = screen.getByText("Hello Jaejoong");
+    expect(textElement).toBeInTheDocument();
+  });
 });
